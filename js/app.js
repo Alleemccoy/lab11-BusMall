26df32691bb813017e15e300036f54ec71793c10
alleemccoy <alleemccoy@hotmail.com>
2021-02-17 23:27:22 -0800

    Completed lab 11

--- js/app.js
+++ js/app.js
@@ -1,70 +1,113 @@
 'use strict';
 
 let allProducts = [];
+let productOne = document.querySelector('section img:first-child');
+let productTwo = document.querySelector('section img:nth-child(2)');
+let productThree = document.querySelector('section img:nth-child(3)');
+let section = document.querySelector('section');
+let buttonResults = document.getElementById('viewResults');
+let timesClicked = 0;
+let allowedClicks = 25;
 
-function Product(name, filePath) {
+function Product(name, fileExtension = 'jpg') {
   this.name = name;
-  this.filePath = filePath;
+  this.src = `images/${name}.${fileExtension}`;
   this.numberOfViews = 0;
   this.numberOfClicks = 0;
   allProducts.push(this);
 }
 
-new Product('bag', 'bag.jpg');
-new Product('banana', 'banana.jpg');
-new Product('bathroom', 'bathroom.jpg');
-new Product('boots', 'boots.jpg');
-new Product('breakfast', 'breakfast.jpg');
-new Product('bubblegum', 'bubblegum.jpg');
-new Product('chair', 'chair.jpg');
-new Product('cthulhu', 'cthulhu.jpg');
-new Product('dog-duck', 'dog-duck.jpg');
-new Product('dragon', 'dragon.jpg');
-new Product('pen', 'pen.jpg');
-new Product('pet-sweep', 'pet-sweep.jpg');
-new Product('scissors', 'scissors.jpg');
-new Product('shark', 'shark.jpg');
-new Product('sweep', 'sweep.png');
-new Product('tauntaun', 'tauntaun.jpg');
-new Product('unicorn', 'unicorn.jpg');
-new Product('usb', 'usb.gif');
-new Product('water-can', 'water-can.jpg');
-new Product('wine-glass', 'wine-glass.jpg');
+new Product('bag');
+new Product('banana');
+new Product('bathroom');
+new Product('boots');
+new Product('breakfast');
+new Product('bubblegum');
+new Product('chair');
+new Product('cthulhu');
+new Product('dog-duck');
+new Product('dragon');
+new Product('pen');
+new Product('pet-sweep');
+new Product('scissors');
+new Product('shark');
+new Product('sweep', 'png');
+new Product('tauntaun');
+new Product('unicorn');
+new Product('usb', 'gif');
+new Product('water-can');
+new Product('wine-glass');
 
-function getRandomIndex() {
+function getRandomProduct() {
   return Math.floor(Math.random() * allProducts.length);
 }
 
+
 function renderProducts() {
-  let firstProductIndex = getRandomIndex();
-  let secondProductIndex = getRandomIndex();
-  let thirdProductIndex = getRandomIndex();
+  let indexArray = [];
+  indexArray[0] = getRandomProduct();
+  indexArray[1] = getRandomProduct();
+  indexArray[2] = getRandomProduct();
 
-  // firstProductIndex = 1;
-  // secondProductIndex = 1;
-  // thirdProductIndex = 1;
-
-  // console.log(firstProductIndex);
-  // console.log(secondProductIndex);
-  // console.log(thirdProductIndex);
-
-  while (firstProductIndex === secondProductIndex) {
-    // console.log('inside while loop');
-    secondProductIndex = getRandomIndex();
+  while (indexArray[0] === indexArray[1]) {
+    indexArray[1] = getRandomProduct();
   }
 
-  while (firstProductIndex === thirdProductIndex) {
-    // console.log('inside while loop');
-    thirdProductIndex = getRandomIndex();
+  while (indexArray[1] === indexArray[2]) {
+    indexArray[2] = getRandomProduct();
   }
 
-  while (secondProductIndex === thirdProductIndex) {
-    // console.log('inside while loop');
-    thirdProductIndex = getRandomIndex();
+  while (indexArray[2] === indexArray[0]) {
+    indexArray[0] = getRandomProduct();
   }
 
-  // console.log(firstProductIndex);
-  // console.log(secondProductIndex);
-  // console.log(thirdProductIndex);
+  productOne.src = allProducts[indexArray[0]].src;
+  productOne.title = allProducts[indexArray[0]].name;
+  allProducts[indexArray[0]].numberOfViews++;
+
+  productTwo.src = allProducts[indexArray[1]].src;
+  productTwo.title = allProducts[indexArray[1]].name;
+  allProducts[indexArray[1]].numberOfViews++;
+
+  productThree.src = allProducts[indexArray[2]].src;
+  productThree.title = allProducts[indexArray[2]].name;
+  allProducts[indexArray[2]].numberOfViews++;
+
 }
 
+// create render function
+
+function renderVoteResults() {
+  let clicks = document.querySelector('ul');
+  for (let i = 0; i < allProducts.length; i++) {
+    let li = document.createElement('li');
+    li.textContent = `${allProducts[i].name} received ${allProducts[i].numberOfClicks} clicks, and was seen ${allProducts[i].numberOfViews} times`;
+    clicks.appendChild(li);
+  }
+}
+
+function clickOnPage(event) {
+  if (event.target === section) {
+    alert('Please click on an image');
+  }
+  timesClicked++;
+  let clickItem = event.target.title;
+  for (let i = 0; i < allProducts.length; i++) {
+    if (clickItem === allProducts[i].name) {
+      allProducts[i].numberOfClicks++;
+    }
+  }
+  renderProducts();
+  if (timesClicked === allowedClicks) {
+    section.removeEventListener('click', clickOnPage);
+  }
+}
+function clickButton(event) {
+  if (timesClicked === allowedClicks) {
+    renderVoteResults();
+  }
+}
+renderProducts();
+
+section.addEventListener('click', clickOnPage);
+buttonResults.addEventListener('click', renderVoteResults);
